0d2704a22fb061e4edea4104b36bfe559ae9a057
Justine B <justinegayton@gmail.com>
2019-01-16 15:23:35 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,9 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def index
-    @posts = Post.all
-  end
+  #def index
+   # @posts = Post.all
+  #end
 
   def new
     @post = Post.new
